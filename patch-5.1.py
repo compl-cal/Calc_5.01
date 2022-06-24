48a05a80e193104e7248b384575636b945af4ede
Suhaa <106807380+suhaarajesh@users.noreply.github.com>
2022-06-24 15:40:18 +0530

    Update patch-5.1.py

--- patch-5.1.py
+++ patch-5.1.py
@@ -82,9 +82,6 @@ def Evaluate():
             lis_operations.pop(0)
         if Error_case == 0:
             final_value = lis_values[0]
-            real_part = round(final_value.real, 3)
-            imag_part = round(final_value.imag, 3)
-            final_value = complex(real_part, imag_part)
             if (final_value.imag == 0):
                 final_value = final_value.real
             label_result.config(text=final_value)
@@ -147,6 +144,8 @@ def Conj():
     else:
         pass
 
+#solve by stat window 2
+
 def Solve_by_stat():
     global Stat_window
     if not Stat_window:
@@ -190,94 +189,107 @@ def Solve_by_stat():
       
 
     Stat_Entry = Entry(Stat_window, width=30, bd =3 )
-    Stat_Entry.grid(row=0, column=0, columnspan=3,pady=5)
+    Stat_Entry.grid(row=0, column=0, columnspan=3,padx=5,pady=10)
 
-    button_solve = Button(Stat_window,image=img_evaluate, text="Solve",command=Solve, bg="#202020", width=50, height=50, borderwidth=0)
+    button_solve = Button(Stat_window,image=img_sample_1, text="Solve",command=Solve, bg="#202020", width=50, height=50, borderwidth=0)
     button_solve.grid(row=1, column=0)
 
-    button_clear_stat = Button(Stat_window, text="Clear",image=img_clear, command=Clear_stat, bg="#202020", width=50, height=50, borderwidth=0)
+    button_clear_stat = Button(Stat_window, text="Clear",image=img_sample_1, command=Clear_stat, bg="#202020", width=50, height=50, borderwidth=0)
     button_clear_stat.grid(row=1, column=1)
 
-    pass_button = Button(Stat_window, width=20, text="Pass value to main window", command=Pass_to_main)
-    pass_button.grid(row=2,column=0,columnspan=2)
+    pass_button = Button(Stat_window, text="Pass value to main window", command=Pass_to_main, image=img_sample_2,  bg="#202020", width=100, height=50, borderwidth=0)
+    pass_button.grid(row=1,column=2,columnspan=2)
 
-    label_result_stat = Label(Stat_window, width=30, height=1, text=stat_val, bg="#C9C7C7")
+    label_result_stat = Label(Stat_window, width=15, height=1, text=stat_val, bg="#C9C7C7")
     label_result_stat.grid(row=3, column=0, columnspan=2, pady=5)
     
     Stat_window.protocol("WM_DELETE_WINDOW", close)
 
+img_sample_1 = PhotoImage(file="C:/Users/Admin/Desktop/proimg/samp_1.png")
+img_sample_2 = PhotoImage(file="C:/Users/Admin/Desktop/proimg/samp_2.png")
+
 Main_window.title('Complex Number Calculator')
 
 label2 = Label(Main_window, text="Real part", width=15, height=2, font=tuple_font, bg="#202020", foreground="#75E9FC")
-label2.grid(row=0, columnspan=1, column=0)
+label2.grid(row=0, columnspan=2, column=0)
 
 real_part = Entry(Main_window, width=20, bd=5)
-real_part.grid(row=1, columnspan=1, column=0)
+real_part.grid(row=1, columnspan=2, column=0)
 
 label1 = Label(Main_window, text="Imaginary part", width=15, height=2, font=tuple_font, bg="#202020", foreground="#75E9FC")
-label1.grid(row=0, columnspan=1, column=1)
+label1.grid(row=0, columnspan=2, column=2)
 
 imag_part = Entry(Main_window, width=20, bd=5)
-imag_part.grid(row=1, columnspan=1, column=1)
+imag_part.grid(row=1, columnspan=2, column=2)
 
 img_add = PhotoImage(file="C:/Users/Admin/Desktop/proimg/plus_8.png")
 
 button_add = Button(Main_window, image=img_add, command=lambda : Operation_func("+"), fg="red", bg="#202020", width=50, height=50, borderwidth=0)
-button_add.grid(row=0, column=2)
+button_add.grid(row=0, column=4)
 
 img_subtract = PhotoImage(file="C:/Users/Admin/Desktop/proimg/minus_3.png")
 
 button_subtract = Button(Main_window, image=img_subtract, command=lambda : Operation_func("-"), fg="red", bg="#202020", width=50, height=50, borderwidth=0)
-button_subtract.grid(row=0, column=3, pady=5,padx=10)
+button_subtract.grid(row=0, column=5, pady=5,padx=10)
 
 img_multiply = PhotoImage(file="C:/Users/Admin/Desktop/proimg/multiply_3.png")
                      
 button_multiply = Button(Main_window, image=img_multiply, command=lambda : Operation_func("*"), fg="red", bg="#202020", width=50, height=50, borderwidth=0)     
-button_multiply.grid(row=1, column=2, pady=5)
+button_multiply.grid(row=1, column=4, pady=5)
 
 img_divide = PhotoImage(file="C:/Users/Admin/Desktop/proimg/divide_3.png")
 
 button_divide = Button(Main_window, image=img_divide, command=lambda : Operation_func("/"), fg="red", bg="#202020", width=50,height=50, borderwidth=0)
-button_divide.grid(row=1, column=3, pady=5)
+button_divide.grid(row=1, column=5, pady=5)
 
 img_clear= PhotoImage(file="C:/Users/Admin/Desktop/proimg/clear_3.png")
 
 button_clear = Button(Main_window, text="Clear", command=Clear,  image=img_clear, fg="red", bg="#202020", width=50,height=50, borderwidth=0)
-button_clear.grid(row=2, column=2, pady=5)
+button_clear.grid(row=2, column=4, pady=5)
 
 img_evaluate = PhotoImage(file="C:/Users/Admin/Desktop/proimg/eval_1.png")
 
 button_evaluate = Button(Main_window, text="=", command=Evaluate, image=img_evaluate, fg="red", bg="#202020", width=50,height=50, borderwidth=0)
-button_evaluate.grid(row=2, column=3, pady=5)
+button_evaluate.grid(row=2, column=5, pady=5)
 
 img_arg= PhotoImage(file="C:/Users/Admin/Desktop/proimg/arg_1.png")
 
 button_arg = Button(Main_window, text="arg()", command=Argument,image=img_arg, fg="red", bg="#202020", width=50,height=50, borderwidth=0)
-button_arg.grid(row=0, column=4, pady=5)
+button_arg.grid(row=0, column=6, pady=5)
 
 img_mod= PhotoImage(file="C:/Users/Admin/Desktop/proimg/mod_1.png")
 
 button_mod = Button(Main_window, text="mod()", command=Modulus, image=img_mod, fg="red", bg="#202020", width=50,height=50, borderwidth=0)
-button_mod.grid(row=1, column=4, pady=5)
+button_mod.grid(row=1, column=6, pady=5)
 
 img_conj = PhotoImage(file="C:/Users/Admin/Desktop/proimg/conj_2.png")
 
 button_conj = Button(Main_window, text="conj()", command=Conj, image=img_conj, fg="red", bg="#202020", width=50,height=50, borderwidth=0)
-button_conj.grid(row=2, column=4, pady=5,)
+button_conj.grid(row=2, column=6, pady=5,)
 
-label_result = Label(Main_window, text=final_value, height=2, bg="#936BFF",fg="white", width=35, font=tuple_font1)
-label_result.grid(row=2,column=0, columnspan=2,pady=10, padx=10)
+img_previous = PhotoImage(file="C:/Users/Admin/Desktop/proimg/left_arrow.png")
+
+button_previous = Button(Main_window, text="Solve by expression", image=img_previous, fg="red", bg="#202020", width=50,height=50, borderwidth=0)
+button_previous.grid(row = 2, column=0, pady=5,padx=5)
+
+img_next = PhotoImage(file="C:/Users/Admin/Desktop/proimg/right_arrow.png")
+
+button_next = Button(Main_window, text="Solve by expression", image=img_next, fg="red", bg="#202020", width=50,height=50, borderwidth=0)
+button_next.grid(row = 2, column=3, pady=5,padx=5)
+
+label_result = Label(Main_window, text=final_value, height=2, bg="#936BFF",fg="white", width=20, font=tuple_font1)
+label_result.grid(row=2,column=1, columnspan=2,pady=10, padx=5)
 
 button_solvebystat = Button(Main_window, text="Solve by expression", command=Solve_by_stat, width=20)
-button_solvebystat.grid(row = 3, column=0, pady=5, columnspan=2,padx=10)
+button_solvebystat.grid(row = 3, column=0, pady=5, columnspan=4,padx=10)
 
 img_power = PhotoImage(file="C:/Users/Admin/Desktop/proimg/exp_4.png")
 
 button_power = Button(Main_window, text="^", command=power, image=img_power,  bg="#202020", width=50,height=50, borderwidth=0)
-button_power.grid(row=3, column=3)
+button_power.grid(row=3, column=5)
 
 label_power = Entry(Main_window, width=5, bd=5)
-label_power.grid(row=3, column=2)
+label_power.grid(row=3, column=4)
 
 Main_window.mainloop()
                    
